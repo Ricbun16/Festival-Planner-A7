14ed0ccae26496155e566ede06927cce66e958a3
swavans <sworms@avans.nl>
2016-02-16 14:10:07 +0100

    iue
    kjhedsjkl

--- src/Schedule.java
+++ src/Schedule.java
@@ -10,11 +10,51 @@ public class Schedule {
 	
 	public Schedule(int startTime, int stopTime)
 	{
-		scheduleStartTime = startTime;
-		scheduleStopTime = stopTime;
+		setScheduleStartTime(startTime);
+		setScheduleStopTime(stopTime);
 	}
 	
 	public void setArtists(String artistName){
+		this.setArtistName(artistName);
+	}
+
+	public ArrayList<Artist> getArtist() {
+		return artist;
+	}
+
+	public void setArtist(ArrayList<Artist> artist) {
+		this.artist = artist;
+	}
+
+	public ArrayList<Stage> getStages() {
+		return stages;
+	}
+
+	public void setStages(ArrayList<Stage> stages) {
+		this.stages = stages;
+	}
+
+	public String getArtistName() {
+		return artistName;
+	}
+
+	public void setArtistName(String artistName) {
 		this.artistName = artistName;
+	}
+
+	public int getScheduleStartTime() {
+		return scheduleStartTime;
+	}
+
+	public void setScheduleStartTime(int scheduleStartTime) {
+		this.scheduleStartTime = scheduleStartTime;
+	}
+
+	public int getScheduleStopTime() {
+		return scheduleStopTime;
+	}
+
+	public void setScheduleStopTime(int scheduleStopTime) {
+		this.scheduleStopTime = scheduleStopTime;
 	}
 }
