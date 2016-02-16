59ed4555a70241617c3232e9bc869aae123c4852
Alkiimista <dducnguyen97@gmail.com>
2016-02-16 12:13:35 +0100

    Begin Schedule class

---
+++ src/Schedule.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Schedule {
+
+	private ArrayList<Artist> artist;
+	private String artistName;
+	
+	public Schedule()
+	{
+	
+	}
+	
+	public void setArtists(String artistName){
+		this.artistName = artistName;
+	}
+}
