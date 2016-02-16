8964e5d6f8323308c2b171ba7d4343119df6ac1a
swavans <sworms@avans.nl>
2016-02-16 13:00:37 +0100

    Added Constructor
    Added consturctor and scheduleMethod

--- src/Stage.java
+++ src/Stage.java
@@ -2,13 +2,36 @@ import java.util.ArrayList;
 
 public class Stage {
 	private ArrayList<Artist> artist = new ArrayList<Artist>;
-	private ArrayList<Integer> timeSlot = new ArrayList<Integer>;
-	private int TimeSlotLength;
+	private ArrayList<TimeSlot> timeSlots = new ArrayList<TimeSlot>;
+	private int timeSlotLength;
 	private String location;
 	private String stageName;
+	private int stageStartTime;
+	private int stageStopTime;
 	
-	public Stage()
+	public Stage(int startTime, int stopTime, int timeSlotLength, String location)
 	{
+		this.location = location;
+		stageStartTime = startTime;
+		stageStopTime = stopTime;
+		this.timeSlotLength = timeSlotLength;
+		int timeSlotStart = stageStartTime;
+		int timeSlotStop;
+		
+		while( timeSlotStart + timeSlotLength < stageStopTime)
+		{
+			timeSlotStop = timeSlotStart + timeSlotLength;
+			TimeSlot timeSlot = new TimeSlot(timeSlotStart, timeSlotStop, false)
+			timeSlots.add(timeSlot);
+			timeSlotStart = timeSlotStop;
+		}
+	}
+
+	public void scheduleArtist(int i, Artist artist) {
+		TimeSlot currentSlot = timeSlots.get(i);
+		currentSlot.setOccupied(true);
+		currentSlot.setArtist(artist);
+		timeSlots.set(i, currentSlot);
 		
 	}
 	
