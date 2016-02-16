de1446f297d40138d2ff30eb303fde015bff79cb
nickbeenackers <nickbeenackers@gmail.com>
2016-02-16 12:59:20 +0100

    timeslot
    met commentaar

--- src/TimeSlot.java
+++ src/TimeSlot.java
@@ -14,38 +14,47 @@ public class TimeSlot {
 		this.isOccupied = isOccupied;
 	}
 	
+	// add an artist to arraylist with artists
 	public void scheduleArtistOnSlot(Artist artist) {
 		artists.add(artist);
 	}
 	
+	// replace arraylist artists with new arraylist
 	public void sceduleArtistsOnSlot(ArrayLsit<Artist> artists) {
 		this.artists = artists;
 	}
 	
+	// return arraylist with artists
 	public ArrayList<Artist> getsceduledAtists() {
 		return artists;
 	}
 	
+	// return boolean isOccupied
 	public boolean checkIsOccupied() {
 		return isOccupied;
 	}
 	
+	// return starting time
 	public int getTimeSlotStart() {
 		return timeSlotStart;
 	}
 	
+	// return ending time
 	public int getTimeSlotEnd() {
 		return timeSlotEnd;
 	}
 	
+	// return populartity
 	public int getPopularity() {
 		return popularity;
 	}
 	
+	// set the boolean isOccupied
 	public void setIsOccupied(boolean isOccupied) {
 		this.isOccupied = isOccupied;
 	}
 
+	// set the integer popularity
 	public void setPopularity(int popularity) {
 		this.popularity = popularity;
 	}
