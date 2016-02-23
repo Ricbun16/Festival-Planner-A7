da2d8c461290a54d8ec108e8a4abe1c21957a5e4
Alkiimista <dducnguyen97@gmail.com>
2016-02-23 13:29:26 +0100

    Berend veranderingen

--- src/GUI.java
+++ src/GUI.java
@@ -22,11 +22,11 @@ public class GUI extends JFrame{
 	
 	private Schedule schedule;
 	private JPanel westBorder;
-	private ArrayList<TimeSlot> timeSlots;
+	private ArrayList<TimeSlot> timeSlots = new ArrayList<TimeSlot>();
 	private short state = 0, buttonState = 0;
  	private String title;
  	private JLabel titleLabel;
-	
+ 	
 	public static void main(String s[]){
 		new GUI();
 	}
@@ -119,23 +119,40 @@ public class GUI extends JFrame{
 	}
 	
 	public void addStagePopUp(){
-		Object[] possibilities = null;
-		String s = (String)JOptionPane.showInputDialog(
-		                    null,
-		                    "Please name the stage",
-		                    "Add stage",
-		                    JOptionPane.PLAIN_MESSAGE,
-		                    null,
-		                    possibilities,
-		                    "Stage name");
-
-		//If a string was returned, say so.
-		if ((s != null) && (s.length() > 0)) {
-			schedule.addStage(s, 600, 1200, 60);
-		    makeButton(s);
-		    return;
+		
+		JTextField field1 = new JTextField();
+		JTextField field2 = new JTextField();
+		JTextField field3 = new JTextField();
+		JTextField field4 = new JTextField();
+		JTextField field5 = new JTextField();
+		JTextField field6 = new JTextField();
+		
+		int hoursStart;
+		int hoursStop;
+		int minuteStart;
+		int minuteSop;
+		int timeSlotLength;
+		String stageName;
+		
+		Object[] message = {"BeginTijd",field1,":",field2
+		"eindtijd",field3,":",field4,
+		"Naam",field5,"TimeSlot Length :",field6
+		};
+		int option = JOptionPane.showConfirmDialog(null, message, "Enter all your values", JOptionPane.OK_CANCEL_OPTION);
+		if(option == JOptionPane.OK_OPTION)
+		{
+			hoursStart = integer.parseInt(field1.getText());
+			minuteStart = integer.parseInt(field2.getText());
+			hoursStop = integer.parseInt(field3.getText());
+			minuteStop = integer.parseInt(field4.getText());
+			stageName = (String)field5.getText();
+			timeSlotLength = integer.parseInt(field6.getText());
+			schedule.addStage(stageName, hoursStart*60+minuteStart,hoursStop*60+minutesStop, timeSlotLength)
+			makeButton(stageName);
+			
 		}
-	}
+		}
+	
 	
 	public void makeButton(String name) {
 	 
@@ -176,14 +193,14 @@ public class GUI extends JFrame{
 			switchState();
 		}
 	}
-//		public void fillTimeslots(Stage currentStage){
-//		timeSlots.clear();
-//		ArrayList<TimeSlot> tempList = currentStage.getTimeSlots();
-//		for(int i = 0;i<tempList.size();i++){
-//			if(!tempList.get(i).getGebruik)
-//				timeSlot.add(tempList.get(i));
-//		}
-//	}
+		public void fillTimeslots(Stage currentStage){
+		timeSlots.clear();
+		ArrayList<TimeSlot> tempList = currentStage.getTimeSlots();
+		for(int i = 0;i<tempList.size();i++){
+			if(!tempList.get(i).checkIsOccupied())
+				timeSlot.add(tempList.get(i));
+		}
+	}
 	
 	public class TableModel extends AbstractTableModel{
 	
@@ -197,19 +214,27 @@ public class GUI extends JFrame{
 		@Override
 		public int getRowCount() {
 			// TODO Auto-generated method stub
-			return 4;
+			if(timeSlots.size()!=0)
+			return timeSlots.size();
+			else 
+			return 1;
 		}
 	
 		
 		public Object getValueAt(int row, int column) {
-			/*TimeSlot T = timeSlots.get(row + 1);
+			TimeSlot t = null;
+			if(timeSlots != null && timeSlots.size()>row)
+				t = timeSlots.get(row)
+			else
+				return "";
+				
 			switch(column){
-			case 0: return  T.getArtist();
-			case 1: return  T.getArtist().getGenre();
+			case 0: return  T.getScheduledArtists().get(0).getName();
+			case 1: return  T.getScheduledArtists().get(0).getGenreString();
 			case 2: return  T.getTimeSlotStart();
 			case 3: return  T.getTimeSlotEnd();
 			case 4: return  T.getPopularity();
-			}*/
+			}
 			return "";
 		}
 		
